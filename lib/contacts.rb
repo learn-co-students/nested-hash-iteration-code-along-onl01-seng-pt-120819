3b221e2f9a72316d081c851a7b2e225a3a16f51b
Andrei Pestenariu <pclandrei@gmail.com>
2019-12-17 00:36:00 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,10 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts["Freddy Mercury"].each do |key, value|
+  if key == :favorite_ice_cream_flavors 
+    value.delete("strawberry")
+  end 
+end
+p contacts
 end
